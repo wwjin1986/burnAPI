d94d311bcf79c8a3a077e278e86ccc0e5ec31f00
Weiwei Jin <weiweijin@proton.attlocal.net>
2019-06-20 11:02:12 -0400

    inital commit

---
+++ src/main/java/com/weiweijin/burn/BurnApplication.java
@@ -0,0 +1,13 @@
+package com.weiweijin.burn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurnApplication.class, args);
+	}
+
+}

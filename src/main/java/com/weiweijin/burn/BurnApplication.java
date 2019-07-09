97d769d1b46345b832eedd85ff232762b42d8052
Weiwei Jin <weiweijin@proton.attlocal.net>
2019-07-09 14:39:14 -0400

    swagger 2 configration

--- src/main/java/com/weiweijin/burn/BurnApplication.java
+++ src/main/java/com/weiweijin/burn/BurnApplication.java
@@ -2,6 +2,18 @@ package com.weiweijin.burn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.google.common.base.Predicate;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import static springfox.documentation.builders.PathSelectors.regex;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class BurnApplication {
@@ -9,5 +21,6 @@ public class BurnApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BurnApplication.class, args);
 	}
+	
 
 }
--- src/main/java/com/weiweijin/burn/configuration/WebMvcCOnfig.java
+++ src/main/java/com/weiweijin/burn/configuration/WebMvcCOnfig.java
@@ -21,18 +21,13 @@ public class WebMvcCOnfig implements WebMvcConfigurer{
 			.allowedHeaders("*");
 		}
 	
-	//config for swagger2
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-	    registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
-	    registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
-	    registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
-	    registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
-	}
-
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	    registry.addResourceHandler("/api/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
-	    registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
+
 }
